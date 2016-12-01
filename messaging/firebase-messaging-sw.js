34fbc7777d82eb137faeec1bf04d28e4584b9f2d
hello <hello@ifnot.cc>
2016-12-01 12:21:52 +0800

    update message sender id

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -2,13 +2,13 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.2/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+  'messagingSenderId': '936388421675'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -26,7 +26,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
   const notificationTitle = 'Background Message Title';
   const notificationOptions = {
     body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    icon: '/messaging/firebase-logo.png'
   };
 
   return self.registration.showNotification(notificationTitle,
